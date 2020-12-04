b25489bbc357cfd0e81ac002b0b7a726499d5ca1
codezerodawn <codezerodawn@gmail.com>
2020-12-05 01:38:12 +0300

    update less1_1.py

--- les1_1.py
+++ les1_1.py
@@ -1,19 +1,10 @@
-# first point of practical task for first lesson "Basic Python"
-a = 10
-b = 5.5
-c = 'Some text'
-d = True
+user_name = input('Let\'s get to know you. What\'s your name?\n>>>')
+computer_greeting = f'Hello! Nice to meet you {user_name}!'
+print(computer_greeting)
 
-print(a, b, c, d)
-
-first_number = input('Enter some integer please\n>>>')
-second_number = input('Enter some float please\n>>>')
-if first_number.isdigit():
-    print('The sum of your integer and variable "a" =', a + first_number)
+user_age = input('Please tell me how old are you?')
+if user_age.isdigit():
+    print('It\'s greate age to become an a programmer!')
 else:
-    print('You have entered not a digit! Please enter an integer!')
-
-user_name = input('Please enter your name\n>>>')
-user_city = input('Please enter city where you was born\n>>>')
-print(f'Dear, {user_name} you were born in {user_city} - the greate city!')
+    print('Be shure that you enter an integer!')
 
