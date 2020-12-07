9584e14c4a6f8d3dfcb1f6c024c49110635bf794
codezerodawn <codezerodawn@gmail.com>
2020-12-07 23:27:57 +0300

    update les1_1.py

--- les1_1.py
+++ les1_1.py
@@ -1,5 +1,6 @@
 NOT_DIGIT = 'Be shure that you entered an integer!'
 
+# 1 item of the task
 user_name = input('Let\'s get to know you. What\'s your name?\n>>>')
 system_message = f'Hello! Nice to meet you {user_name}!'
 print(system_message)
@@ -10,6 +11,7 @@ if user_age.isdigit():
 else:
     print(NOT_DIGIT)
 
+# 2 item of the task
 time_to_convert = input('Enter the time in seconds so I can convert it in Hours:Minutes:Seconds.\n>>>')
 if time_to_convert.isdigit():
     hours = int(time_to_convert) // 3600
@@ -19,9 +21,10 @@ if time_to_convert.isdigit():
 else:
     print(NOT_DIGIT)
 
+# 3 item of the task
 some_number = input('Let\'s count next example: n+nn+nnn. Enter an arbitrary number.\n>>>')
 n = some_number
-nn  = some_number + some_number
+nn = some_number + some_number
 nnn = some_number + some_number + some_number
 if n.isdigit() and nn.isdigit() and nnn.isdigit():
     count = int(n) + int(nn) + int(nnn)
@@ -30,6 +33,7 @@ if n.isdigit() and nn.isdigit() and nnn.isdigit():
 else:
     print(NOT_DIGIT)
 
+# 4 item of the task
 positive_integer = input('Enter some positive integer, and I\'ll find the largest one digit!\n>>>')
 large_digit = 0
 if positive_integer.isdigit():
@@ -41,28 +45,30 @@ if positive_integer.isdigit():
         positive_integer = int(positive_integer / 10)
 else:
     print(NOT_DIGIT)
-print(f'The largest number is {result}.')
+print(f'The largest number is {large_digit}.')
 
-revenue_value = input('Enter the income value wich your firm have earned in last year, in rubles.\n>>>')
-costs_value = input('Enter the costs value wich your firm have spend in last year, in rubles.\n>>>')
+# 5 item of the task
+revenue_value = input('Enter the income value wich your firm have earned, in rubles.\n>>>')
+costs_value = input('Enter the costs value wich your firm have spend, in rubles.\n>>>')
 if revenue_value.isdigit() and costs_value.isdigit():
     revenue_value = int(revenue_value)
     costs_value = int(costs_value)
     if revenue_value > costs_value:
         profit_value = revenue_value - costs_value
         profitability = round(profit_value / revenue_value * 100, 2)
-        system_message = f'Your\'s firm had profit in last year, the value of profit was - {profit_value} rubles.\n Profitability of the firms revenue is {profitability}%.'
-        print(system_message)
+        system_message = f'Profitability of the revenue is {profitability}%.'
+        print(f'Your\'s firm had profit in last year  - {profit_value}rubles.\n', system_message)
     elif revenue_value < costs_value:
         loss_value = costs_value - revenue_value
-        system_message = f'In last year your firm suffered losses in value - {loss_value} rubles. You have to find the way to make up for losses.'
+        system_message = f'In last year your firm suffered losses in value - {loss_value} rubles.'
         print(system_message)
     else:
-        system_message = 'Your firm  didn\'t yearn money last year, the revenue and costs was equal. You shoud change some to become profitable.'
+        system_message = 'The revenue and costs was equal. You shoud change some to become profitable.'
         print(system_message)
 else:
     print(NOT_DIGIT)
 
+# 6 item of thq task
 a = input('Enter the number of kilometers that you have done the first day of training.\n>>>')
 b = input('Enter the number of kilometers that you want to handle with per day.\n>>>')
 day_number = 1
