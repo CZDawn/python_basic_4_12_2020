8de000e6558c8b3b45c7bc83e165125202df4cdf
codezerodawn <codezerodawn@gmail.com>
2020-12-16 23:30:11 +0300

    done task 4

--- les3_task4.py
+++ les3_task4.py
@@ -5,28 +5,19 @@
 Задание необходимо реализовать в виде функции my_func(x, y).
 При решении задания необходимо обойтись без встроенной функции возведения числа в степень.
 
-Подсказка: попробуйте решить задачу двумя способами.
-Первый — возведение в степень с помощью оператора **.
-Второй — более сложная реализация без оператора **, предусматривающая использование цикла.
-
 '''
 
-def my_func(x, y):
-    if x.isdigit() and y.isdigit():
-        x = int(x)
-        y = int(y)
-        if y < 0:
-            return (x ** -(y))
-        elif y > 0:
-            return x ** y
-        else:
-            return 1
-    else:
-        print('You shoud enter an integers.')
+def my_multip(x, y):
+    result = 0
+    for _ in range(y):
+        result += x
+    return result
 
-variable_x = input('Enter a positive number:\n>>>')
-variable_y = input('Enter a negative number:\n>>>')
+def my_funck(x: float, y: int):
+    result = 1
+    for _ in range(abs(y)):
+        result = my_multip(result, x)
+    return result if y > 0 else 1 / result
 
-result = my_func(variable_x, variable_y)
-print(result)
+print(my_funck(2, -4))
 
