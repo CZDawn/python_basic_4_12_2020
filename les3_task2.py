1a3a533556b49deff022492a348ae747f8831548
codezerodawn <codezerodawn@gmail.com>
2020-12-17 15:56:32 +0300

    update task 2

--- les3_task2.py
+++ les3_task2.py
@@ -7,18 +7,33 @@
 
 '''
 
-def info(**kwargs):
-    result = name + ' ' + surname + ', ' + year_of_birth + ', ' + city_of_living + ', ' + email + ', ' + phone + '.'
+def user_info(**kwargs):
+    ''' Return string with user data
+
+    named arguments (**kwargs):
+    name -- user name (default - string)
+    surname -- user surname (default - string)
+    year -- user year of birth (default - string)
+    city -- user city of living (default - string)
+    email -- user email (default - string)
+    phone -- user phone number (defaultt - string)
+
+    >>> user_info(name='Ivan', surname='Ivanov', year_of_birth='2000', city='Moskow', email='info@gmail.com', phone='+7900000000')
+    Ivan Ivanov, 2000, Moskow, info@gmail.com, +79000000000
+    '''
+
+    result = name + ' ' + surname + ', ' + year + ', ' + city + ', ' + email + ', ' + phone + '.'
     return result
+
 
 name = input('What is your name?\n>>>')
 surname = input('What is your surname?\n>>>')
-year_of_birth = input('When you were born? (year)\n>>>')
-city_of_living = input('Where you live?\n>>>')
+year = input('When you were born? (year)\n>>>')
+city = input('Where you live?\n>>>')
 email = input('Enter your e-mail.\n>>>')
 phone = input('Enter your phone number.\n>>>')
 
-user_data = info(arg_1=name, arg_2=surname, arg_3=year_of_birth, arg_4=city_of_living, arg_5=email, arg_6=phone)
+
+user_data = user_info(arg_1=name, arg_2=surname, arg_3=year, arg_4=city, arg_5=email, arg_6=phone)
 print(user_data)
-print(type(user_data))
 
