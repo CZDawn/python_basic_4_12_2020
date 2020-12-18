c218102016b0c50aae6cf870293e5e92d8b88588
codezerodawn <codezerodawn@gmail.com>
2020-12-18 18:10:21 +0300

    done task 2

--- les4_task2.py
+++ les4_task2.py
@@ -7,8 +7,12 @@
 
 my_list = list(input('Enter some integers devided by spaces.\n>>>').split())
 my_list = [int(el) for el in my_list if el.isdigit()]
-new_list = [el + 10 for el in my_list]
+
+new_list = []
+for i in range(len(my_list)-1):
+    if my_list[i] < my_list[i+1]:
+        new_list.append(my_list[i+1])
 
 print(f'Default list: {my_list}')
-print(f'New list (aded 10 to elements): {new_list}')
+print(f'New list: {new_list}')
 
