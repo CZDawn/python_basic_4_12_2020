da27087bd47d7e2c9e646307b4fe280532525c7e
codezerodawn <codezerodawn@gmail.com>
2020-12-19 20:29:49 +0300

    update task 2

--- les4_task2.py
+++ les4_task2.py
@@ -5,5 +5,11 @@
 
 '''
 
+default_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+result_list = []
+for i in range(len(default_list)-1):
+    if default_list[i] < default_list[i+1]:
+        result_list.append(default_list[i+1])
 
+print(result_list)
 
